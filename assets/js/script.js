9a51b7435d74afa2c08917090f3cf3dea0a8591f
bronislava111 <vladlena456@bk.ru>
2021-12-08 00:06:51 +0600

    v

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+function replaceone(Type) {
+    document.getElementById("div3").style.display = (Type) ? "block" : 'none';
+    document.getElementById("div4").style.display = (Type) ? "none" : "block";
+};
